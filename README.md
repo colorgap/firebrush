a323c29d9ef1382aecbd3fc8a42d5780bc14609b
Color][gap <admin@colorgap.com>
2016-10-08 21:55:56 -0400

    readme changes and material design used

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # firebowyer
 Bowyer architecture and ease of development on firebase with angular
+
+> git clone git@github.com:colorgap/firebowyer.git
+
+> cd firebowyer
+
+> npm install
+
+> npm start
--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import angularMaterial from 'angular-material';
 
 import '../styles/app.scss';
 
@@ -20,8 +21,43 @@ class AppCtrl {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+  let configBlock = function($mdThemingProvider) {
+      $mdThemingProvider.definePalette('bowyer-purple', {
+        '50': 'D1B5F4',
+        '100': 'A872EB',
+        '200': '8A41E4',
+        '300': '661CC2',
+        '400': '5818A7',
+        '500': '4A148C',
+        '600': '3C1071',
+        '700': '2E0C56',
+        '800': '20093C',
+        '900': '110521',
+        'A100': 'D1B5F4',
+        'A200': 'A872EB',
+        'A400': '5818A7',
+        'A700': '2E0C56',
+        'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                            // on this palette should be dark or light
+        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+        '200', '300', '400', 'A100'],
+        'contrastLightColors': ['500', '600', //hues which contrast should be 'dark' by default
+        '700', '800', '900', 'A400']    // could also specify this if default was 'dark'
+    });
+
+    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+    $mdThemingProvider.theme('amber').backgroundPalette('amber');
+    
+    $mdThemingProvider.theme('bowyer')
+        .primaryPalette('bowyer-purple')
+        .accentPalette('amber');
+    $mdThemingProvider.setDefaultTheme('bowyer');
+  };
+  angular.module(MODULE_NAME, [angularMaterial])
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller('AppCtrl', AppCtrl)
+  .config(configBlock);
 
 export default MODULE_NAME;
