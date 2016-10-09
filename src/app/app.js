12cf424ccaa93fcf64a038c948fd4371e8024d17
Color][gap <admin@colorgap.com>
2016-10-08 21:08:05 -0400

    angular and material changes

---
+++ src/app/app.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+
+import '../styles/app.scss';
+
+let app = () => {
+  return {
+    template: require('../partials/index.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'appCtrl'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    var appCtrl = this;
+    appCtrl.url = 'https://github.com/preboot/angular-webpack';
+      console.log(appCtrl);
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
